3ac48605ff2edcc11897b9d1e9d3725a5c6c8d1c
Ryan Hays <hays.ryan@gmail.com>
2020-02-28 08:05:54 -0800

    Added Posh and Metasploit C2 installers

--- CommandAndControl/c2_server.sh
+++ CommandAndControl/c2_server.sh
@@ -40,7 +40,7 @@ TOTAL=$(grep '(${STAGE}/${TOTAL})' $0 | wc -l);(( TOTAL-- ))
 STARTTIME=$(date +%s)
 export STAGING_KEY="RANDOM"
 export DEBIAN_FRONTEND="noninteractive"
-AVAIL_C2=(posh)
+AVAIL_C2=(posh msf)
 
 
 ##### PRE CHECKS #####
@@ -58,7 +58,8 @@ fi
 if [ "$1" == "" ]; then
   echo -e " ${RED}[!]${RESET} You must specify a type of C2 framework to deploy."
   echo -e " ${RED}[!]${RESET} Available Frameworks: "
-  printf '%s\n' "${AVAIL_C2[@]^^}"
+  echo -e "     ${YELLOW}[i]${RESET} posh     PoshC2 - https://github.com/nettitude/PoshC2"
+  echo -e "     ${YELLOW}[i]${RESET} msf      Metasploit - https://github.com/rapid7/metasploit-framework"
   exit 1
 fi
 C2SERVER=${1^^}
@@ -119,26 +120,166 @@ else
   echo -e " ${YELLOW}[i]${RESET} ${YELLOW}Detected Internet access${RESET}" 1>&2
 fi
 
-CONTINUE='N'
-echo -e " ${BLUE}[*]${RESET} ${BOLD}Before continuing with this script make sure you have two servers setup Phishing and a Redirector along with\n those required IPs. You should also have the DNS entries also specified within your configuraiton.\nContinue (y/N): ${RESET}"
-read -r CONTINUE
-if [[ "${CONTINUE^^}" == "N" ]]; then
-  exit
-fi
 
 echo -e " ${BLUE}[*]${RESET} ${BOLD}Enter your hostname/primary domain name eg: c2.example.com: ${RESET}"
 read -r PRIDOMAIN
 
 ##### FUNCTIONS #####
 ##### Initial Ubuntu Config
-if [[ ! -f /root/.phish_firstrun ]]; then
+if [[ ! -f /root/.c2_firstrun ]]; then
   (( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}Initial Ubuntu Configuration${RESET}"
   echo -e "\n ${YELLOW}[i]${RESET} ${YELLOW}Installing Updates${RESET}"
-  touch /root/.phish_firstrun
+  touch /root/.c2_firstrun
 
   ### Perfomring system updates
   apt-get -qq update >/dev/null 2>&1
   apt-get -qq -y upgrade >/dev/null 2>&1
   apt-get -qq -y dist-upgrade >/dev/null 2>&1
   apt-get -qq -y autoremove >/dev/null 2>&1
+
+  ### Disabling IPv6
+  update-rc.d nfs-common disable >/dev/null 2>&1
+  update-rc.d rpcbind disable >/dev/null 2>&1
+
+  cat <<-EOF >> /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+net.ipv6.conf.eth0.disable_ipv6 = 1
+net.ipv6.conf.eth1.disable_ipv6 = 1
+net.ipv6.conf.ppp0.disable_ipv6 = 1
+net.ipv6.conf.tun0.disable_ipv6 = 1
+EOF
+
+  sysctl -p >/dev/null 2>&1
+
+  ### Updating hostname
+  cat <<-EOF > /etc/hosts
+127.0.1.1 $PRIDOMAIN $PRIDOMAIN
+127.0.0.1 localhost
+EOF
+
+  cat <<-EOF > /etc/hostname
+$PRIDOMAIN
+EOF
+
+  ### Bash Aliases
+  cat <<-EOF > /root/.bash_aliases
+## grep aliases
+alias grep="grep --color=always"
+alias ngrep="grep -n"
+
+alias egrep="egrep --color=auto"
+
+alias fgrep="fgrep --color=auto"
+
+## Checksums
+alias sha1="openssl sha1"
+alias md5="openssl md5"
+
+## List open ports
+alias ports="netstat -tulanp"
+
+## Get external IP address
+alias ipx="curl -s http://ipinfo.io/ip"
+
+## Directory navigation aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+## Update the OS
+alias update-os='apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt autoremove -y'
+
+## Extract file
+## Extract file, example. "ex package.tar.bz2"
+ex() {
+  if [[ -f \$1 ]]; then
+    case \$1 in
+      *.tar.bz2) tar xjf \$1 ;;
+      *.tar.gz)  tar xzf \$1 ;;
+      *.bz2)     bunzip2 \$1 ;;
+      *.rar)     rar x \$1 ;;
+      *.gz)      gunzip \$1  ;;
+      *.tar)     tar xf \$1  ;;
+      *.tbz2)    tar xjf \$1 ;;
+      *.tgz)     tar xzf \$1 ;;
+      *.zip)     unzip \$1 ;;
+      *.Z)       uncompress \$1 ;;
+      *.7z)      7z x \$1 ;;
+      *)         echo \$1 cannot be extracted ;;
+    esac
+  else
+    echo \$1 is not a valid file
+  fi
+}
+
+## openvas
+alias openvas="openvas-stop; openvas-start; sleep 3s; xdg-open https://127.0.0.1:9392/ >/dev/null 2>&1"
+EOF
+
+  echo -e "\n ${YELLOW}[i]${RESET} ${YELLOW}The System will now reboot!${RESET}"
+  echo -e "\n ${YELLOW}[i]${RESET} ${YELLOW}Please rerun this script after the system comes back online.${RESET}"
+  sleep 5
+  reboot
+else
+  rm -rf /root/.c2_firstrun >/dev/null 2>&1
+  echo -e "\n ${YELLOW}[i]${RESET} ${YELLOW}First run of the script already performed skipping this step.${RESET}"
+
+  ##### SSH Configuration
+  (( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}SSH Configuration${RESET}"
+  systemctl stop ssh.service >/dev/null 2>&1
+  mkdir /etc/ssh/default_keys >/dev/null 2>&1
+  mv /etc/ssh/ssh_host_* /etc/ssh/default_keys/
+  dpkg-reconfigure openssh-server >/dev/null 2>&1
+  sed -i "s/PermitRootLogin yes/PermitRootLogin without-password/g" /etc/ssh/sshd_config
+  systemctl enable ssh.service >/dev/null 2>&1
+  systemctl start ssh.service >/dev/null 2>&1
+
+  case $C2SERVER in
+    POSH)
+      curl -sSL https://raw.githubusercontent.com/nettitude/PoshC2/master/Install.sh | bash
+      ;;
+    MSF)
+      apt-get install -qq -y autoconf bison build-essential curl git-core libapr1 libaprutil1 libcurl4-openssl-dev \
+      libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev \
+      libxslt-dev libyaml-dev locate ncurses-dev openssl postgresql postgresql-contrib wget xsel zlib1g \
+      zlib1g-dev >/dev/null 2>&1 \
+      || echo -e "\n ${RED}[!] Issue with apt install${RESET}"
+      pushd /tmp
+      wget -O - https://apt.metasploit.com/metasploit-framework.gpg.key | apt-key add -
+      wget --quiet -L -O msfinstall https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
+      chmod 755 msfinstall
+      ./msfinstall
+
+      update-rc.d postgresql enable
+      sudo -u postgres msfdb init --user postgres --password password
+
+      popd >/dev/null
+      ;;
+  esac
 fi
+
+##### CLEANUP #####
+##### Clean the system
+(( STAGE++ )); echo -e "\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) ${GREEN}Cleaning${RESET} the system"
+#--- Clean package manager
+for FILE in clean autoremove; do apt-get -y -qq "${FILE}"; done
+apt-get -y -qq purge $(dpkg -l | tail -n +6 | egrep -v '^(h|i)i' | awk '{print $2}')   # Purged packages
+#--- Update slocate database
+updatedb
+#--- Reset folder location
+cd ~/ &>/dev/null
+#--- Remove any history files (as they could contain sensitive info)
+history -c 2>/dev/null
+for i in $(cut -d: -f6 /etc/passwd | sort -u); do
+[[ -e "${i}" ]] && find "${i}" -type f -name '.*_history' -delete
+done
+
+##### Time taken
+FINISHTIME=$(date +%s)
+
+cat dnsentries.txt
+echo -e "\n ${YELLOW}[i]${RESET} Time (roughly) taken: ${YELLOW}$(( $(( FINISHTIME - STARTTIME )) / 60 )) minutes${RESET}"
+echo -e "\n ${YELLOW}[i]${RESET} Please reboot the system now to ensure all changes are taken. ${YELLOW}${RESET}"
